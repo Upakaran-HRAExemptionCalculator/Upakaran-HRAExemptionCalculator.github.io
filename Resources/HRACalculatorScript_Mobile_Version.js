f98b9a43ece721371c5a705a3a8f4739224e330a
Upakaran-HRAExemptionCalculator <kaushik.0407@gmail.com>
2020-05-06 23:13:35 +0530

    js change

--- Resources/HRACalculatorScript_Mobile_Version.js
+++ Resources/HRACalculatorScript_Mobile_Version.js
@@ -535,8 +535,8 @@
 		//	}
 		//	};
 
-			window.addEventListener("resize", doOnOrientationChange, false);
-			window.addEventListener("orientationchange", doOnOrientationChange, false);
+		//	window.addEventListener("resize", doOnOrientationChange, false);
+		//	window.addEventListener("orientationchange", doOnOrientationChange, false);
 			screen.orientation.addEventListener("change", doOnScreenOrientationChange , false );
 			
 			// (optional) Android doesn't always fire orientationChange on 180 degree turns
