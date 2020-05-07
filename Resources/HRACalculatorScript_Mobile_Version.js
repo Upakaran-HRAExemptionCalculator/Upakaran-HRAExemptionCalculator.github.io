03ed044504260ef8f531bc2e88dcd9b61d66a411
Upakaran-HRAExemptionCalculator <kaushik.0407@gmail.com>
2020-05-07 22:36:08 +0530

    changes

--- Resources/HRACalculatorScript_Mobile_Version.js
+++ Resources/HRACalculatorScript_Mobile_Version.js
@@ -798,9 +798,9 @@
 							if(currentOrientation != prevOrientation && currentOrientation.includes("landscape") && orientationOnLastLoad.includes("landscape")){
 								console.log("portrait");
 								console.log("device held vertically.");
-								document.getElementsByName("firstHeading")[0].style.fontSize = 'xx-large';
-								document.getElementsByName("secondHeading")[0].style.fontSize = 'large';
-								document.getElementsByName("thirdHeading")[0].style.fontSize = 'large';
+								document.getElementsByName("firstHeading")[0].style.fontSize = 'xxx-large';
+								document.getElementsByName("secondHeading")[0].style.fontSize = 'x-large';
+								document.getElementsByName("thirdHeading")[0].style.fontSize = 'x-large';
 							
 							//	document.getElementsByName("tips")[0].style.fontSize = 'x-small';
 							//	document.getElementById("downloadGuide").style.fontSize = 'x-small';
@@ -818,7 +818,7 @@
 								
 							//	document.getElementsByName("developerInfo")[0].style.fontSize = 'x-small';
 								document.getElementsByName("developerInfo")[0].style.fontSize = 'medium';
-								document.getElementsByName("footer")[0].style.fontSize = 'smaller';
+								document.getElementsByName("footer")[0].style.fontSize = 'medium';
 							}
 							
 											
