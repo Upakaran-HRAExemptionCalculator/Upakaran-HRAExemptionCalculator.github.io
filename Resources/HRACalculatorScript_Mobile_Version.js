c27732869b48464e0f8ecf295328f80e8a2c69fb
Upakaran-HRAExemptionCalculator <kaushik.0407@gmail.com>
2020-05-08 12:25:50 +0530

    changes

--- Resources/HRACalculatorScript_Mobile_Version.js
+++ Resources/HRACalculatorScript_Mobile_Version.js
@@ -720,15 +720,19 @@
 								document.getElementsByName("firstHeading")[0].style.fontSize = 'xx-large';
 								document.getElementsByName("secondHeading")[0].style.fontSize = 'large';
 								document.getElementsByName("thirdHeading")[0].style.fontSize = 'large';
-								document.getElementsByName("tips")[0].style.fontSize = 'medium';
-								document.getElementById("downloadGuide").style.fontSize = 'medium';
-								document.getElementById("tryDownload").style.fontSize = 'medium';
+							//	document.getElementsByName("tips")[0].style.fontSize = 'medium';
+							//	document.getElementById("downloadGuide").style.fontSize = 'medium';
+							//	document.getElementById("tryDownload").style.fontSize = 'medium';
+								document.getElementsByName("tips")[0].style.fontSize = 'x-small';
+								document.getElementById("downloadGuide").style.fontSize = 'x-small';
+								document.getElementById("tryDownload").style.fontSize = 'x-small';
 								document.getElementsByName("YourName")[0].style.fontSize = 'large';
 								document.getElementsByName("YourName")[0].style.width = '300px';
 								document.getElementsByName("FY")[0].style.fontSize = 'large';
 								document.getElementsByName("FY")[0].style.width = '180px';
 								document.getElementsByName("generatePDF")[0].style.fontSize = 'large';
-								document.getElementsByName("developerInfo")[0].style.fontSize = 'medium';
+							//	document.getElementsByName("developerInfo")[0].style.fontSize = 'medium';
+								document.getElementsByName("developerInfo")[0].style.fontSize = 'x-small';
 								document.getElementsByName("footer")[0].style.fontSize = 'smaller';
 								previousOrientation = "landscape";
 								
